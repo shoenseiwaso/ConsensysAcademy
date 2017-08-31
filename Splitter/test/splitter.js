eef246f8a44433348d40d6a1f4602dc81745412f
Jeff Wentworth <jeff@curvegrid.com>
2017-08-31 10:16:04 +0900

    in testing, MetaCoin => Splitter

--- Splitter/test/splitter.js
+++ Splitter/test/splitter.js
@@ -1,6 +1,6 @@
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+var Splitter = artifacts.require("./Splitter.sol");
 
-contract('MetaCoin', function(accounts) {
+contract('Splitter', function(accounts) {
   it("should put 10000 MetaCoin in the first account", function() {
     return MetaCoin.deployed().then(function(instance) {
       return instance.getBalance.call(accounts[0]);
