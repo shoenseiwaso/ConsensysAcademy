69d8abe12c81b9461ed2a26043945a7ff1acdc68
Jeff Wentworth <jeff@curvegrid.com>
2017-09-04 13:04:13 +0900

    Fix various splitter lite test issues

--- Splitter/test/splitterlite.js
+++ Splitter/test/splitterlite.js
@@ -23,126 +23,41 @@ contract('SplitterLite', function(accounts) {
   });
 
   it("Alice splits an even amount of wei between Bob and Carol", function () {
-    var fromBalBefore = eth.getBalance(u.alice);
-    var to1BalBefore = eth.getBalance(u.bob);
-    var to2BalBefore = eth.getBalance(u.carol);
+    var fromBalBefore = web3.eth.getBalance(u.alice);
+    var to1BalBefore = web3.eth.getBalance(u.bob);
+    var to2BalBefore = web3.eth.getBalance(u.carol);
+
+    // compute expected balances
+    var value = testValueEven;
+    var valueTo1 = Math.floor(value / 2);
+    var valueTo2 = value - valueTo1 + 1;
 
     return contract.split(
       u.bob,
       u.carol,
-      {from: u.alice, value: testValueEven})
+      {from: u.alice, value: value})
     .then(function(txn) {
-      return contract.balances(u.bob);
-      
-      // compute expected balances
-      var value = testValueEven;
-      var valueTo1 = Math.floor(value / 2);
-      var valueTo2 = value - valueTo1;
-      var fromBalAfter = fromBalBefore.minus(value);
-      var to1BalAfter = to1BalBefore.minus(valueTo1);
-      var to2BalAfter = to1BalBefore.minus(valueTo2);
-
-      assert.strictEqual(eth.getBalance(u.alice), fromBalAfter, "Alice's expected balance doesn't match");
-      assert.strictEqual(eth.getBalance(u.alice), fromBalAfter, "Alice's expected balance doesn't match");
-      assert.strictEqual(eth.getBalance(u.alice), fromBalAfter, "Alice's expected balance doesn't match");
-
-      // compute expected balances, handling case where test value is an odd number
-      var user2ExpectedBalance = Math.floor(testValueEven / 2);
-      var user1ExpectedBalance = testValueEven - user2ExpectedBalance;
-
-      var _fromUserAddr = v[0];
-      var _fromUserName = v[1];
-      var _toUser1Addr = v[2];
-      var _toUser1Name = v[3];
-      var _toUser1Balance = v[4];
-      var _toUser2Addr = v[5];
-      var _toUser2Name = v[6];
-      var _toUser2Balance = v[7];
-
-      assert.equal(_fromUserAddr.toString(10), u.alice.addr.toString(10), "Incorrect from user address");
-      assert.equal(_fromUserName, u.alice.name, "Incorrect from user name");
-      assert.equal(_toUser1Addr.toString(10), u.bob.addr.toString(10), "Incorrect to user 1 address");
-      assert.equal(_toUser1Name, u.bob.name, "Incorrect to user 1 name");
-      assert.equal(_toUser1Balance.toString(10), user1ExpectedBalance, "Incorrect to user 1 balance");
-      assert.equal(_toUser2Addr.toString(10), u.carol.addr.toString(10), "Incorrect to user 2 address");
-      assert.equal(_toUser2Name, u.carol.name, "Incorrect to user 2 name");
-      assert.equal(_toUser2Balance.toString(10), user2ExpectedBalance, "Incorrect to user 2 balance");
-    });
-  });
-
-  // For handling throws with geth via Truffle, see: https://stackoverflow.com/a/40386159/3649726
-  // Note that this eventually will not (reliably) work once Solidity is updated so that 
-  // revert() behaves differently from throw and returns unused gas to caller.
-  it("should refuse to add a duplicate splitter (same 'from' address)", function () {
-    return contract.insertSplitter(
-      u.alice.addr, 
-      u.alice.name, 
-      u.bob.addr, 
-      u.bob.name, 
-      u.carol.addr, 
-      u.carol.name,
-      {from: u.alice.addr, value: testValueEven})
-    .then(function(txn) {
-      return contract.insertSplitter(
-        u.alice.addr, 
-        u.alice.name, 
-        u.bob.addr, 
-        u.bob.name, 
-        u.carol.addr, 
-        u.carol.name,
-        {from: u.alice.addr, value: testValueEven});
-    })
-    .then(function(txn) {
+      // check that split() didn't throw by running out of gas
       var tx = web3.eth.getTransaction(txn.tx);
       var txr = txn.receipt;
-      assert.strictEqual(txr.gasUsed, tx.gas, "Not all gas was used up, transaction did not throw.");
-    });
-  });
+      assert.notStrictEqual(txr.gasUsed, tx.gas, "All gas was used up, split() threw an exception.");
 
-  it("should add a valid splitter with Alice, Bob and Carol, then a second one with Bob, David and Emma, with an odd amount of wei", function () {
-    return contract.insertSplitter(
-      u.alice.addr, 
-      u.alice.name, 
-      u.bob.addr, 
-      u.bob.name, 
-      u.carol.addr, 
-      u.carol.name,
-      {from: u.alice.addr, value: testValueEven})
-    .then(function(txn) {
-        return contract.insertSplitter(
-          u.bob.addr, 
-          u.bob.name, 
-          u.david.addr, 
-          u.david.name, 
-          u.emma.addr, 
-          u.emma.name,
-          {from: u.bob.addr, value: testValueOdd});
-      })
-    .then(function(txn) {
-      return contract.getSplitterAtIndex(1);
+      // check Alice's balance
+      // note that this simple check works because her account is the etherbase,
+      // i.e., transaction fees are a wash
+      assert.strictEqual(web3.eth.getBalance(u.alice).plus(value).toString(10), fromBalBefore.toString(10), "Alice's expected balance doesn't match");
+
+      return contract.balances(u.bob);
     })
-    .then(function(v) {
-      // compute expected balances, handling case where test value is an odd number
-      var user2ExpectedBalance = Math.floor(testValueOdd / 2);
-      var user1ExpectedBalance = testValueOdd - user2ExpectedBalance;
+    .then(function(to1BalAfter) {
+      assert.equal(to1BalAfter.toString(10), to1BalBefore.plus(valueTo1).toString(10), "Bob's expected balance doesn't match");
 
-      var _fromUserAddr = v[0];
-      var _fromUserName = v[1];
-      var _toUser1Addr = v[2];
-      var _toUser1Name = v[3];
-      var _toUser1Balance = v[4];
-      var _toUser2Addr = v[5];
-      var _toUser2Name = v[6];
-      var _toUser2Balance = v[7];
+      return contract.balances(u.carol);
+    })
+    .then(function(to2BalAfter) {
+      assert.strictEqual(to2BalAfter.toString(10), to2BalBefore.plus(valueTo2).toString(10), "Carol's expected balance doesn't match");
 
-      assert.equal(_fromUserAddr.toString(10), u.bob.addr.toString(10), "Incorrect from user address");
-      assert.equal(_fromUserName, u.bob.name, "Incorrect from user name");
-      assert.equal(_toUser1Addr.toString(10), u.david.addr.toString(10), "Incorrect to user 1 address");
-      assert.equal(_toUser1Name, u.david.name, "Incorrect to user 1 name");
-      assert.equal(_toUser1Balance.toString(10), user1ExpectedBalance, "Incorrect to user 1 balance");
-      assert.equal(_toUser2Addr.toString(10), u.emma.addr.toString(10), "Incorrect to user 2 address");
-      assert.equal(_toUser2Name, u.emma.name, "Incorrect to user 2 name");
-      assert.equal(_toUser2Balance.toString(10), user2ExpectedBalance, "Incorrect to user 2 balance");
+      assert.strictEqual(1, 2, "Something is broken");
     });
   });
 });
