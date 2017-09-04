3678ee552f2cc4db33e9752fc1f834c13e1ef34e
Jeff Wentworth <jeff@curvegrid.com>
2017-09-05 02:09:28 +0900

    add events to splitter lite

--- Splitter/contracts/SplitterLite.sol
+++ Splitter/contracts/SplitterLite.sol
@@ -22,27 +22,33 @@ contract SplitterLite {
 	// global state variables will be set on contract creation
 	address public owner = msg.sender;
 
+	event Split(address indexed _from, address indexed _to1, address indexed _to2, uint _amount);
+	event Withdraw(address indexed _to, uint _amount);
+
 	// accept funds from owner 
-	function split(address to1, address to2) public payable {
+	function split(address to1, address to2) public payable returns (bool success) {
 		uint amount1 = msg.value / 2;
 		balances[to1] += amount1;
 		balances[to2] += msg.value - amount1;
+		return true;
 	}
 
 	// use the preferred withdrawal pattern rather than the send pattern
 	// see: http://solidity.readthedocs.io/en/develop/common-patterns.html#withdrawal-from-contracts
-	function withdraw() {
+	function withdraw() public returns (bool success) {
         uint amount = balances[msg.sender];
         balances[msg.sender] = 0;
         msg.sender.transfer(amount);
+		return true;
     }
 
 	// Kill the contract and return remaining balance to the owner.
 	// Clearly this will disenfranchise anyone with an outstanding balance,
 	// so either the owner needs to be a trusted party or this function should be 
 	// expanded with safeguards.
-	function kill() public { 
+	function kill() public returns (bool success) { 
 		require(msg.sender == owner);
 		selfdestruct(owner);
+		return true;
 	}
 }
